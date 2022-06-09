5a8102bcaa5416e1f138ebf97b6814e09cbfab48
wanglj <wanglijian1993>
2022-06-09 17:57:47 +0800

    提交C++基础

--- 博客/编译so/交叉编译脚本.sh
+++ 博客/编译so/交叉编译脚本.sh
@@ -1,11 +1,11 @@
-rm -rf build 
+rm -r build
 mkdir build
 cd build
 #设置交叉编译的参数
-cmake DANDROID_NDK=${NDK} \
-        .DCHAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/ndroid.toolchain.cmake \
-        -DANDROID_ABI="armeabi-v8a" \
+cmake -DANDROID_NDK=${NDK_PATH} \
+	    -DCMAKE_TOOLCHAIN_FILE=${NDK_PATH}/build/cmake/android.toolchain.cmake \
+	    -DANDROID_ABI="arm64-v8a" \
         -DANDROID_NATIVE_API_LEVEL=21 \
         ..
-
+        
 make
